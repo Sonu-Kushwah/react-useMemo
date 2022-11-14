317b74f33cbf70c46b9d17fbbb379503031aeca1
learnReactgithub <sonukushwah362@gmail.com>
2022-11-14 21:02:38 +0530

    use memo hooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { useMemo, useState } from 'react';
 
 function App() {
+  const [count,setCount]=useState(1);
+  const [item,setItem]=useState(10);
+  
+  const multiCountItem=useMemo(function multicoutn(){
+    console.log("multicount");
+    return count*2;
+  },[count])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <h3>Use Memo Hooks</h3>
+     <h4>Count:{count}</h4>
+     <h4>Item:{item}</h4>
+     <h4>{multiCountItem}</h4>
+     <button onClick={()=>setCount(count+1)}>count</button>
+     <button onClick={()=>setItem(item*2)}>item</button>
+    </>
   );
 }
 
